bf8173eec6895cfcebf93961ba7756f0e81a1a64
mkfreeman <michael.k.freeman@gmail.com>
2016-02-18 14:15:08 -0800

    Simplified shiny example

--- shiny-example/server.R
+++ shiny-example/server.R
@@ -1,10 +1,5 @@
 library(shiny)
 
 shinyServer(function(input, output) {
-  # Create a histogram property of the output
-  output$histogram <- renderPlot({
-    # Use shiny's renderPlot function to render a plot
-    x <- rnorm(input$num)
-    return(hist(x))
-  })
+ 
 })
--- shiny-example/ui.R
+++ shiny-example/ui.R
@@ -4,15 +4,6 @@ library(shiny)
 shinyUI(
   fluidPage(
     # Application title
-    titlePanel("Hello Shiny!"), 
-    
-    # Create a mainPanel of the app
-    mainPanel(
-      # Add a numbericInput to choose the number of observations
-      numericInput("num", label = "# observations", value = 100),
-      
-      # Plot the output with the name "histogram"
-      plotOutput('histogram')
-    )
+    titlePanel("Hello Shiny!")
   )
 )
