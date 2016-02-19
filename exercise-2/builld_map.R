d341d2d4096553379264606e9aaaad742b6d98f8
mkfreeman <michael.k.freeman@gmail.com>
2016-02-18 16:50:37 -0800

    Added exercise-2

---
+++ exercise-2/builld_map.R
@@ -0,0 +1,29 @@
+### Example from plotly ###
+# Original: https://plot.ly/r/choropleth-maps/
+
+### Steps:
+# 1.  Put this map into a Shiny application 
+# 2.  Add a select box that allows you to select which column is on the map (i.e., beef, dairy, total.fruits, etc.)
+
+# Tutorial on adding widgets: http://shiny.rstudio.com/tutorial/lesson3/
+# Shiny widget gallery: http://shiny.rstudio.com/gallery/widget-gallery.html
+
+library(plotly)
+df <- read.csv("https://raw.githubusercontent.com/plotly/datasets/master/2011_us_ag_exports.csv")
+df$hover <- with(df, paste(state, '<br>', "Beef", beef, "Dairy", dairy, "<br>",
+                           "Fruits", total.fruits, "Veggies", total.veggies,
+                           "<br>", "Wheat", wheat, "Corn", corn))
+# give state boundaries a white border
+l <- list(color = toRGB("white"), width = 2)
+# specify some map projection/options
+g <- list(
+  scope = 'usa',
+  projection = list(type = 'albers usa'),
+  showlakes = TRUE,
+  lakecolor = toRGB('white')
+)
+
+plot_ly(df, z = total.exports, text = hover, locations = code, type = 'choropleth',
+        locationmode = 'USA-states', color = total.exports, colors = 'Purples',
+        marker = list(line = l), colorbar = list(title = "Millions USD")) %>%
+  layout(title = '2011 US Agriculture Exports by State<br>(Hover for breakdown)', geo = g)
