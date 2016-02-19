11a79a71b8d3d18315679319f092f0d94375449e
mkfreeman <michael.k.freeman@gmail.com>
2016-02-18 17:15:03 -0800

    Simplified exercise 2

---
+++ exercise-2/make_plot.R
@@ -0,0 +1,11 @@
+### Exercise 2 ###
+
+# 1.  Build a shiny app that displays this chart
+# 2.  Create number inputs that let you choose the number of points
+# 3.  Bonus: Create a dropdown menu to let the user pick the color:
+
+# We'll look into these more next week: http://shiny.rstudio.com/gallery/widget-gallery.html 
+
+x <- rnorm(100)
+y <- rnorm(100)
+plot(x,y, col='blue')
